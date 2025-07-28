a53df47e2ec8f7f137804f55608a9cda2aa14253
getfjell <tim@getfjell.com>
2025-07-27 20:09:47 -0700

    chore: bump version to 4.4.34
     (#61)
    
    * - Restore local file linking for development dependencies in package manifests
    - Switch "@fjell/eslint-config" in root package.json from npm version to local file reference for local development and testing workflows
    - Switch "@fjell/docs-template" in docs/package.json from npm version to local file reference for development and rapid iteration
    - Add .kodrdriv-link-backup.json to store backup link metadata for local package linking
    - Expand public API surface in src/index.ts by re-exporting getLogger, types (Logger, TimeLogger, LoggingConfig, FloodControlConfig), and namespaced enums LogLevel and LogFormat from their respective modules
    
    * Switch @fjell/eslint-config and @fjell/docs-template dependencies from local file references back to published npm versions
    
    - Update @fjell/eslint-config in root package.json devDependencies from a local file reference to "^1.1.3"
    - Update @fjell/docs-template in docs/package.json dependencies from a local file reference to "^1.0.24"
    - Remove .kodrdriv-link-backup.json, as backup metadata for local linking is no longer needed
    
    * chore: bump version to 4.4.34
    
    ---------
    
    Co-authored-by: Fjell Project <info@getfjell.com>

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
 import { getLogger } from "./logging";
-export type { Logger } from "./Logger";
+
+export { getLogger };
+export type { Logger, TimeLogger } from "./Logger";
+export type { LoggingConfig } from "./config";
+export type { FloodControlConfig } from "./FloodControl";
+export * as LogLevel from "./LogLevel";
+export * as LogFormat from "./LogFormat";
 
 export default { getLogger };
